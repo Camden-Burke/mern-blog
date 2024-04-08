31a3d3966fbbdb09f3980770f43d2a3f7e8bb3e9
Camden-Burke <camdenburke20@gmail.com>
2024-04-08 15:28:04 -0400

    Started Work on Header Component
    Added Header Component with a logo and a search form with a textinput with a responsive button for smaller screens

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,10 +6,12 @@ import Home from './pages/Home';
 import Projects from './pages/Projects';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
+import Header from './components/Header';
 
 export default function App() {
   return (
     <BrowserRouter>
+    <Header />
     <Routes>
     <Route path='/about' element={<About />} />
     <Route path='/dashboard' element={<Dashboard />} />
